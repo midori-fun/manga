84a35210d829a5fe69c39b8685de9e5c9241a518
jiyu <m@jiyu.lol>
2018-04-24 22:21:59 +0900

    mm

--- public/js/lazy_load.js
+++ public/js/lazy_load.js
@@ -2,14 +2,19 @@ let all_img = $(".read_manga img");
 let img_len = all_img.length;
 let loaded_counter = 0;
 
-for(let i=0; i<img_len; i++){
-	all_img[i].addEventListener("load",loadFunc);
-	$(".read_manga img").eq(0).attr("src", function() {
+for(let i=0; i<img_len; i++) {
+	all_img[i].addEventListener("load", loadFunc);
+	$(".read_manga a, .read_manga img").eq(0).attr("src", function() {
 		return $(this).attr("data");
-	}).css("display", "none").fadeIn(500);
-	$(".read_manga img").eq(1).attr("src", function() {
+	}).css("display", "none").removeClass("not_displayed");
+	$(".read_manga img").eq(0).on("load", function() {
+		$(this).fadeIn(500, function() {
+			$(".read_manga a, .read_manga img").eq(1).css("visibility", "");
+		});
+	});
+	$(".read_manga a, .read_manga img").eq(1).attr("src", function() {
 		return $(this).attr("data");
-	}).css("display", "none");
+	}).css("visibility", "hidden");
 }
 
 function loadFunc() {
@@ -22,7 +27,7 @@ function loadFunc() {
 
 $(window).on("scroll", function() {
 	let targets = $('.not_displayed');
-	targets.find("img").css("display", "none");
+	targets.find("a, img").css("display", "none");
 
 	targets.each(function(i, e) {
 		let t = $(e).offset().top;
@@ -31,7 +36,7 @@ $(window).on("scroll", function() {
 
 		if($(window).scrollTop() > p) {
 			$(e).removeClass("not_displayed");
-			$(e).find("img").css("display", "");
+			$(e).find("a, img").fadeIn(500);
 
 			$(`#page_${nb_page+1} img, #page_${nb_page+2} img`).attr("src", function() {
 				return $(this).attr("data");
@@ -39,3 +44,5 @@ $(window).on("scroll", function() {
 		}
 	});
 });
+
+$(window).scroll();
